bf80961d5d0fc2ee32f2c70e9827b4e809a43c36
aclown <guitarfreak@gmx.net>
2013-02-28 15:14:04 +0100

    Merge r2364
    Changes suggested by DevHC:
    - Remove "!target" checks from tell, vtell, and gc commands. Target was just set, cannot be NULL.
    - Validate "order" in gc command before "player id."

--- code/game/g_cmds.c
+++ code/game/g_cmds.c
@@ -941,7 +941,7 @@ static void Cmd_Tell_f(gentity_t* ent) {
     }
 
     target = &g_entities[targetNum];
-    if (!target || !target->inuse || !target->client) {
+    if (!target->inuse || !target->client) {
         return;
     }
 
@@ -1062,7 +1062,7 @@ static void Cmd_VoiceTell_f(gentity_t* ent, qboolean voiceonly) {
     }
 
     target = &g_entities[targetNum];
-    if (!target || !target->inuse || !target->client) {
+    if (!target->inuse || !target->client) {
         return;
     }
 
@@ -1176,6 +1176,14 @@ void Cmd_GameCommand_f(gentity_t* ent) {
         return;
     }
 
+    trap_Argv(2, arg, sizeof(arg));
+    order = atoi(arg);
+
+    if (order < 0 || order >= numgc_orders) {
+        trap_SendServerCommand(ent - g_entities, va("print \"Bad order: %i\n\"", order));
+        return;
+    }
+
     trap_Argv(1, arg, sizeof(arg));
     targetNum = ClientNumberFromString(ent, arg);
     if (targetNum == -1) {
@@ -1183,15 +1191,7 @@ void Cmd_GameCommand_f(gentity_t* ent) {
     }
 
     target = &g_entities[targetNum];
-    if (!target || !target->inuse || !target->client) {
-        return;
-    }
-
-    trap_Argv(2, arg, sizeof(arg));
-    order = atoi(arg);
-
-    if (order < 0 || order >= numgc_orders) {
-        trap_SendServerCommand(ent - g_entities, va("print \"Bad order: %i\n\"", order));
+    if (!target->inuse || !target->client) {
         return;
     }
 
