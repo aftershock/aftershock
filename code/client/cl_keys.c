78b5fed76d004aaed59c4eace49ff2a4e1572c45
aclown <guitarfreak@gmx.net>
2013-03-05 19:07:30 +0100

    Merge ioq3 commit 9c701a7
    Run toggleconsole in binds while in menu or message mode

--- code/client/cl_keys.c
+++ code/client/cl_keys.c
@@ -1111,6 +1111,8 @@ static qboolean CL_BindUICommand(const char* cmd) {
     if (Key_GetCatcher() & KEYCATCH_CONSOLE)
         return qfalse;
 
+    if (!Q_stricmp(cmd, "toggleconsole"))
+        return qtrue;
     if (!Q_stricmp(cmd, "togglemenu"))
         return qtrue;
 
